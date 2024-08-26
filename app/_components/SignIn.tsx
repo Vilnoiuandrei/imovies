a340053dfd41a8b59ab0f21fc4d97e9b25b391b4
Andrei Vilnoiu <Vilnoiuandrei@gmail.com>
2024-08-27 00:11:36 +0300

    signin works nice

--- app/_components/SignIn.tsx
+++ app/_components/SignIn.tsx
@@ -21,34 +21,35 @@ export default function SignIn() {
 
       {/* Google Sign In */}
       <button
-        className="bg-customLight text-customDark mt-3 h-10 w-24 shadow-md rounded-sm"
+        className="mt-5 h-10 w-52 shadow-md rounded-sm bg-white text-black"
         onClick={() => signIn("google", { callbackUrl: "/account" })}
       >
         Sign In with Google
       </button>
+      <p className="p-2">or</p>
 
       {/* Email Sign In */}
       <form
         onSubmit={handleEmailSignIn}
-        className="mt-4 flex flex-col items-center"
+        className=" flex flex-col items-center"
       >
         <input
           type="email"
           name="email"
           placeholder="Email"
           required
-          className="p-2 border-b-2 border-gray-300 focus:outline-none focus:border-customDark mt-2"
+          className="p-2 border-b-2 border-gray-300 focus:outline-none focus:border-black mt-2 w-52 "
         />
         <input
           type="password"
           name="password"
           placeholder="Password"
           required
-          className="p-2 border-b-2 border-gray-300 focus:outline-none focus:border-customDark mt-2"
+          className="p-2 border-b-2 border-gray-300 focus:outline-none focus:border-black mt-2 w-52 "
         />
         <button
           type="submit"
-          className="bg-customLight text-customDark mt-3 h-10 w-24 shadow-md rounded-sm"
+          className="bg-white text-black mt-3 h-10 w-52 shadow-md rounded-sm"
         >
           Sign In with Email
         </button>
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,7 +2,8 @@ import SignIn from "../_components/SignIn";
 import { auth } from "../_lib/auth";
 
 export default async function Account() {
-  // const session = await auth();
+  const session = await auth();
+  console.log(session);
 
   return (
     <div className="flex justify-center">
